7af773f57e4d490a6b51b13d416aa5da8ad15f0b
dennismutia <dennis.mutia@gmail.com>
2021-07-20 09:27:47 +0300

    refactor: combine two CTEs into one for readability

--- vouchers_project_dbt/models/marts/weekly_redeemed_vouchers_cumulative.sql
+++ vouchers_project_dbt/models/marts/weekly_redeemed_vouchers_cumulative.sql
@@ -11,28 +11,20 @@ Returns:        A table containing the cumulative number of redeemed vouchers ev
     )
 }}
 
-with redeemed_vouchers as (
+with weekly_redemptions as (
     select 
         year,
         month_name as month,
         week_of_year,
-        m.date,
-        voucher_code
+        min(m.date) as start_of_week,
+        max(m.date) as end_of_week,
+        v.status,
+        count(distinct voucher_code) as no_of_redeemed_vouchers
     from {{ ref('master_calendar') }} m
     left join public.vouchers  v
         on m.date = v.date
-    where v.status = 'redeemed'
-),
-weekly_redemptions as (
-    select 
-        year,
-        month,
-        week_of_year,
-        min(date) as start_of_week,
-        max(date) as end_of_week,
-        count(distinct voucher_code) as no_of_redeemed_vouchers
-    from redeemed_vouchers
-    group by year, month, week_of_year
+    group by year, month_name, week_of_year, v.status
+    having v.status = 'redeemed'
 )
 select
     *,
