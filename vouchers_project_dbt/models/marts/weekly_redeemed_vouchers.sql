e6a5379f1e590f5d581b67c50e48cf71e275fe0d
dennismutia <dennis.mutia@gmail.com>
2021-07-20 09:29:47 +0300

    refactor: modify to remove unnecessary CTE

--- vouchers_project_dbt/models/marts/weekly_redeemed_vouchers.sql
+++ vouchers_project_dbt/models/marts/weekly_redeemed_vouchers.sql
@@ -11,25 +11,16 @@ Returns:        A table containing the number of redeemed vouchers per week
     )
 }}
 
-
-with redeemed_vouchers as (
-    select 
-        year,
-        month_name as month,
-        week_of_year,
-        m.date,
-        voucher_code
-    from {{ ref('master_calendar') }} m
-    left join public.vouchers  v
-        on m.date = v.date
-    where v.status = 'redeemed'
-)
 select 
     year,
-    month,
+    month_name as month,
     week_of_year,
-    min(date) as start_of_week,
-    max(date) as end_of_week,
+    min(m.date) as start_of_week,
+    max(m.date) as end_of_week,
+    v.status,
     count(distinct voucher_code) as no_of_redeemed_vouchers
-from redeemed_vouchers
-group by year, month, week_of_year
+from {{ ref('master_calendar') }} m
+left join public.vouchers  v
+    on m.date = v.date
+group by year, month_name, week_of_year, v.status
+having v.status = 'redeemed'
